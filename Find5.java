bb430d15d9329d326d1fdcdf599b6d532f2a1e7d
reallygoodaccount1 <jonah@slackorama.com>
2023-01-19 22:38:42 -0800

    timestable

--- Find5.java
+++ Find5.java
@@ -1,9 +1,7 @@
-package Find5;
-
 class Find5 {
     public static void main(String[] args) {
         int[][] arr = new int[3][4];
-        
+
         for (int a = 0; a < arr.length; a++) {
             for (int b = 0; b < arr[0].length; b++) {
                 arr[a][b] = (int) (Math.random() * 10);
